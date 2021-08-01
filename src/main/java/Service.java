e7f93bbb50487f05ed560e1c9ff1d5299111ef3b
codingmilk <61626865+codingmilk@users.noreply.github.com>
2021-08-01 20:55:03 +0100

    improved code

--- src/main/java/Service.java
+++ src/main/java/Service.java
@@ -1,52 +1,56 @@
-import org.springframework.boot.*;
-import org.springframework.boot.autoconfigure.*;
-import org.springframework.stereotype.*;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.time.Duration;
+import java.time.ZonedDateTime;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 import java.util.ArrayList;
-import java.time.*;
 import java.time.temporal.ChronoUnit;
-import java.util.*;
 import java.text.DecimalFormat;
 
 @RestController
 @EnableAutoConfiguration
 public class Service {
+    // values
     static ArrayList<Integer> pulsometer = new ArrayList();
     static ArrayList<Integer> r = new ArrayList();
     static ArrayList<Integer> g = new ArrayList();
     static ArrayList<Integer> b = new ArrayList();
-    static ArrayList<Float> EngineEff = new ArrayList();
-
-
-    String pattern = "###.##";
-    DecimalFormat decimalFormat = new DecimalFormat(pattern);
+    static ArrayList<Float> engineEff = new ArrayList();
 
     @CrossOrigin
     @RequestMapping("/data")
-    Map home(Service s) {
+    public Map<String, Object> home(Service s) {
         // REST API Service Method here
         ZonedDateTime now = ZonedDateTime.now();
         ZonedDateTime midnight = now.truncatedTo(ChronoUnit.DAYS);
         Duration duration = Duration.between(midnight,now);
         int secondsPassed = (int) duration.getSeconds();
 
-        HashMap map = new HashMap();
-        map.put("pulsometer", pulsometer.get(secondsPassed));
-        map.put("r", r.get(secondsPassed));
-        map.put("g", g.get(secondsPassed));
-        map.put("b", b.get(secondsPassed));
-        map.put("EngineEff",decimalFormat.format(EngineEff.get(secondsPassed)));
-        return map;
+        HashMap<String, Object> values = new HashMap<>();
 
+        values.put("pulsometer", pulsometer.get(secondsPassed));
+        values.put("r", r.get(secondsPassed));
+        values.put("g", g.get(secondsPassed));
+        values.put("b", b.get(secondsPassed));
+
+        String pattern = "###.##";
+        DecimalFormat decimalFormat = new DecimalFormat(pattern);
+        values.put("EngineEff",decimalFormat.format(engineEff.get(secondsPassed)));
+
+        return values;
     }
 
-    public static void loadData(){
-        String filename = "./dashBoardData.csv";
-        File file = new File(filename);
-        String[] values = null;
+    public static void loadData(String fileName){
+        File file = new File(fileName);
+        String[] values;
         boolean skipHeader = true;
 
         try{
@@ -56,7 +60,7 @@ public class Service {
                 if ( skipHeader ) { skipHeader = false; continue;}                
                 values = data.split(",");
                 pulsometer.add(Integer.parseInt(values[1]));
-                EngineEff.add(Float.parseFloat(values[2]));
+                engineEff.add(Float.parseFloat(values[2]));
                 r.add(Integer.parseInt(values[3]));
                 g.add(Integer.parseInt(values[4]));
                 b.add(Integer.parseInt(values[5]));
@@ -67,8 +71,9 @@ public class Service {
         }
     }
 
-    public static void main(String[] args) throws Exception {
-        loadData();
+    public static void main(String[] args) {
+        String fileName = "./dashBoardData.csv";
+        loadData(fileName);
         SpringApplication.run(Service.class, args);
     }
 
